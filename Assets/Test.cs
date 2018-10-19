9fe19f5310563535f84ec1efebb835f0e944d97e
klcatha <klcatha0518@gmail.com>
2018-10-19 09:36:16 +0900

    add script

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,7 +6,8 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		//Hello, Worldをコンソールウィンドウに表示する
+		Debug.Log("Hello, World");		
 	}
 	
 	// Update is called once per frame
