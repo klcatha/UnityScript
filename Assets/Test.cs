6fcdcc03c1e23cbc37dc6cb10ce8ff22615202e0
klcatha <klcatha0518@gmail.com>
2018-10-20 11:49:41 +0900

    calc vector2

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -23,10 +23,13 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Vector2 playerPos = new Vector2(3.0f, 4.0f);
-		playerPos.x += 8.0f;
-		playerPos.y += 5.0f;
-		Debug.Log(playerPos);
+		Vector2 startPos = new Vector2(2.0f, 1.0f);
+		Vector2 endPos = new Vector2(8.0f, 5.0f);
+		Vector2 dir = endPos - startPos;
+		Debug.Log(dir);
+
+		float len = dir.magnitude;
+		Debug.Log(len);
 	}
 	
 	// Update is called once per frame
